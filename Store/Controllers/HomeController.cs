7e16e0efa9206889e4b8ccd38196d76ebc70a26d
zhenyastufeev09012001 <zhenyastufeev09012001@gmail.com>
2018-11-03 23:20:38 +0200

    header

--- Store/Controllers/HomeController.cs
+++ Store/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +10,24 @@ namespace Store.Controllers
 {
     public class HomeController : Controller
     {
+        public ActionResult SomeEvent()
+        {
+            return View();
+        }
         public HomeController(/*IRepository BusinessLogicLayer*/)
         {
 
+        }
+
+        [HttpGet]
+        public string[] GetCategory()
+        {
+            List<string> list = new List<string>();
+            list.Add("Action1");
+            list.Add("Action2");
+            list.Add("Action3");
+            list.Add("Action4");
+            return list.ToArray();
         }
 
         public ActionResult Index()
